c91069af6bf7bd128ada50c13cdeddc0da91efbf
Marcos Leite <m.abner@live.co.uk>
2024-04-22 00:34:44 -0300

    deconstructor use

--- Program.cs
+++ Program.cs
@@ -1,18 +1,26 @@
 ﻿using Tuplas_Operador_Ternario_Desconstrucao_de_Objeto_CSharp.Models;
 
-ReadFile file = new ReadFile();
+Person p1 = new Person("Marcos", "Leite");
 
-var(success, _, lineCount) = file.ReadTxt("Files/SPEDFiscal.txt");
+(string name, string surname) = p1;
 
-if(success)
-{
-    Console.WriteLine($"Line count:{lineCount}");
-    // foreach(var line in fileLines)
-    // {
-    //     Console.WriteLine(line);
-    // }
-}
-else
-{
-    Console.WriteLine("Cannot read the file");
-}
+Console.WriteLine($"{name} {surname}");
+
+
+
+// ReadFile file = new ReadFile();
+
+// var (success, _, lineCount) = file.ReadTxt("Files/SPEDFiscal.txt");
+
+// if (success)
+// {
+//     Console.WriteLine($"Line count:{lineCount}");
+//     // foreach(var line in fileLines)
+//     // {
+//     //     Console.WriteLine(line);
+//     // }
+// }
+// else
+// {
+//     Console.WriteLine("Cannot read the file");
+// }
